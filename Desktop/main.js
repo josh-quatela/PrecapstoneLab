31c078bb93590788d6b20f40be1bd4e2ec3e461e
Joshua Quatela <joshuaquatela@10-21-247-254.dynapool.wireless.nyu.edu>
2021-04-29 16:43:22 -0400

    Added switch to display photeic spelling.

--- Desktop/main.js
+++ Desktop/main.js
@@ -6,7 +6,39 @@ var output = "";
 for (i=0; i < newInts.length; ++i) {
   // Iterates through each digit of the current number
   for (j=0; j < newInts[i].length; ++j) {
-    // TODO
+    // Switch function that adds correct phonetic spelling of the digit
+    // to the output
+    switch(newInts[i][j]) {
+      case "1":
+        output += "One";
+        break;
+      case "2":
+        output += "Two";
+        break;
+      case "3":
+        output += "Three";
+        break;
+      case "4":
+        output += "Four";
+        break;
+      case "5":
+        output += "Five";
+        break;
+      case "6":
+        output += "Six";
+        break;
+      case "7":
+        output += "Seven";
+        break;
+      case "8":
+        output += "Eight";
+        break;
+      case "9":
+        output += "Nine";
+        break;
+      case "0":
+        output += "Zero";
+        break;
     }
   }
 }
