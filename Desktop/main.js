258a527e851d2803d655342fd2d1b2549d6d9244
Joshua Quatela <joshuaquatela@10-21-247-254.dynapool.wireless.nyu.edu>
2021-04-29 16:40:45 -0400

    Parsed input and set up loops

---
+++ Desktop/main.js
@@ -0,0 +1,12 @@
+// Grabs the ints the user passed in
+var newInts = process.argv.slice(2);
+var output = "";
+
+// Iterates through the numbers passed in
+for (i=0; i < newInts.length; ++i) {
+  // Iterates through each digit of the current number
+  for (j=0; j < newInts[i].length; ++j) {
+    // TODO
+    }
+  }
+}
